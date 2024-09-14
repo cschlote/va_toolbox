b29b20b879e7f225f44afe23303039fdcff26932
Carsten Schlote <schlote@vahanus.net>
2024-09-14 16:17:56 +0200

    Removed obsolete test code

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,11 +4,3 @@ set -ex
 # Compile and run the programm in unittest mode
 dub test
 
-# Check the code
-cd source
-dmd -g -O -c hashed_enum.d
-objdump -S hashed_enum.o
-
-ldc2 -g -O -c hashed_enum.d -of=hashed_enum.o2
-objdump -S hashed_enum.o2
-
